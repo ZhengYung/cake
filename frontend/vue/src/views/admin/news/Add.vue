ad0bc7062ecf569a4aef8c704fc647fc086620ca
Zheng <mocheng123598617@gamil.com>
2024-03-26 16:03:30 +0800

    20240326

--- frontend/vue/src/views/admin/news/Add.vue
+++ frontend/vue/src/views/admin/news/Add.vue
@@ -2,18 +2,17 @@
 import { ckeditor, ClassicEditor, editorData, editorConfig } from '@/plugins/ckeditor';
 import { ref, inject, onUnmounted, computed } from 'vue';
 import { useRouter } from 'vue-router';
+import Swal from 'sweetalert2';
+import VuePictureCropper, { cropper } from 'vue-picture-cropper';
+
 const router = useRouter();
 const axios = inject('axios');
-import Swal from 'sweetalert2';
-import { toRaw } from 'vue';
 
 onUnmounted(() => {
     editorData.value = "";
 });
-const dateVal = ref(null);
-const form = ref(false);
-const title = ref("");
-const sequence = ref(null);
+
+
 const rules = {
     required: value => !!value || '必填',
     min_three: value => value.length >= 3 || '至少3個字',
@@ -24,10 +23,14 @@ const rules = {
     }
 }
 
+const form = ref(false);
+const title = ref("");
+
+//datepicker
+const dateVal = ref(null);
 const yesterday = new Date();
 yesterday.setDate(yesterday.getDate() - 1);
 const minDate = yesterday;
-
 
 const dateDisp = computed(() => {
     if (dateVal.value) {
@@ -43,13 +46,92 @@ const closeDatePicker = () => {
     showDatePicker.value = false;
 }
 
+//縮圖
+const cropperStyle = {
+    width: '200px',
+    height: '200px',
+    backgroundColor: '#f8f8f8',
+    margin: 'auto',
+}
+const cropperOptions = {
+    viewMode: 0,
+    dragMode: 'move',
+    aspectRatio: 1,
+    cropBoxResizable: false,
+}
+const cropperPresetMode = {
+    mode: 'fixedSize',
+    width: 200,
+    height: 200,
+}
+
+const dialog = ref(false);
+const uploadInput = ref(null);
+const pic = ref('');
+const thumbnail = ref('');
+
+// 將選擇的檔案傳給cropper
+const selectFile = (e) => {
+    pic.value = '';
+    thumbnail.value = '';
+
+    // 獲取選擇的檔案
+    const { files } = e.target
+    if (!files || !files.length) return
+
+    // 將檔案轉換成 *dataURL* 傳送給 cropper component
+    const file = files[0]
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
+
+    reader.onload = () => {
+        //更新cropper `:img` 的source來顯示圖片以做裁切
+        pic.value = String(reader.result)
+
+        // 開啟dialog
+        dialog.value = true
+
+        //清空element input 選取的檔案
+        if (!uploadInput.value) return
+        uploadInput.value.value = ''
+    }
+}
+
+// 獲取裁切好的結果  
+const getResult = async () => {
+    if (!cropper) return
+    const base64 = cropper.getDataURL()
+    const blob = await cropper.getBlob()
+    const file = await cropper.getFile();
+    if (!blob) return
+
+    const formData = new FormData();
+    formData.append('upload', file);
+    axios.post('http://localhost:3000/image', formData)
+        .then(res => {
+            thumbnail.value = res.data.url;
+        })
+        .catch(err => console.log(`axios錯誤:${err.response.data.message}`));
+    console.log({ base64, blob, file })
+
+    dialog.value = false
+}
+
+//確認cropper有work
+const ready = () => {
+    console.log('Cropper is ready.')
+}
+
+//送出表單
+
 const submitForm = () => {
     console.log('送出表單');
     const formData = {
         data: {
+            Layer:layer.value,
             Title: title.value,
             Content: editorData.value,
-            Sequence: sequence.value
+            Thumbnail:thumbnail.value,
         }
     }
     console.log(editorData.value);
@@ -73,25 +155,46 @@ const submitForm = () => {
 <template>
 
     <v-container :fluid="true">
+        <v-dialog v-model="dialog" transition="dialog-bottom-transition" max-width="400">
+            <v-card>
+                <v-toolbar>
+                    <v-btn icon="fa-solid fa-xmark" @click="dialog = false"></v-btn>
+                    <v-toolbar-title>裁切</v-toolbar-title>
+                    <v-spacer></v-spacer>
+                    <v-toolbar-items>
+                        <v-btn text="取消" variant="text" @click="dialog = false"></v-btn>
+                        <v-btn text="確定" variant="text" @click="getResult"></v-btn>
+                    </v-toolbar-items>
+                </v-toolbar>
+                <div>
+                    <VuePictureCropper :boxStyle="cropperStyle" :img="pic" :options="cropperOptions" @ready="ready"
+                        :presetMode="cropperPresetMode" />
+                </div>
+            </v-card>
+        </v-dialog>
         <v-form @submit.prevent="submitForm" v-model="form">
             <v-row>
                 <v-col cols="3"></v-col>
                 <v-col cols="6">
                     <v-text-field v-model:model-value="title" counter maxlength="20"
-                        :rules="[rules.required, rules.min_three]" label="標題" clearable
-                        hide-details="auto"></v-text-field>
+                        :rules="[rules.required, rules.min_three]" label="標題" clearable></v-text-field>
                     <ckeditor :editor="ClassicEditor" v-model="editorData" :config="editorConfig">
                     </ckeditor>
-                    <v-btn class="d-block ms-auto mt-3 submitBtn" :disabled="!form" type="submit">送出</v-btn>
+                    <v-btn class="d-block ms-auto mt-3 submitBtn" type="submit" :disabled="!form || result==''">送出</v-btn>
                 </v-col>
                 <v-col cols="3">
                     <v-text-field @click:control="showDatePicker = !showDatePicker" v-model:model-value="dateDisp"
-                        label="發佈日" readonly :rules="[rules.required]" hide-details="auto"></v-text-field>
+                        label="發佈日" readonly :rules="[rules.required]"></v-text-field>
                     <div v-if="showDatePicker">
                         <v-date-picker :min="minDate" v-model:model-value="dateVal" width="100%" hide-header
                             v-click-outside="closeDatePicker"></v-date-picker>
                     </div>
-
+                    <div class="select-picture">
+                        <p v-if="result == ''">請選擇縮圖</p>
+                        <img :src="thumbnail" alt="">
+                        <input ref="uploadInput" class="fileSelector" type="file"
+                            accept="image/jpg, image/jpeg, image/png, image/gif" @change="selectFile">
+                    </div>
                 </v-col>
             </v-row>
         </v-form>
@@ -101,5 +204,38 @@ const submitForm = () => {
 :deep(.ck-editor__editable_inline:not(.ck-comment__input *)) {
     height: 500px;
     overflow-y: auto;
+}
+
+.select-picture {
+    position: relative;
+    border: blue dashed 1px;
+    overflow: hidden;
+    width: 200px;
+    aspect-ratio: 1;
+
+    //用來顯示選擇檔案的提示文字
+    p {
+        position: absolute;
+        inset: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    //隱藏input
+    input {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        font-size: 0;
+        cursor: pointer;
+        opacity: 0;
+    }
+}
+
+:deep(.cropper-bg) {
+    background-repeat: repeat !important;
 }
 </style>
